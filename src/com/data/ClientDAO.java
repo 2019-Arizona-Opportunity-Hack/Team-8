88bebccc5e3f334c6a10f00fdd1e83114a626b56
Ruben Cerrato <37849206+RubenC-18@users.noreply.github.com>
2019-10-19 13:49:18 -0700

    Implemented read method.

--- src/com/data/ClientDAO.java
+++ src/com/data/ClientDAO.java
@@ -1,5 +1,11 @@
 package com.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.model.Client;
 import com.model.DTO;
 import com.model.Database;
@@ -11,7 +17,37 @@ public class ClientDAO implements ClientDataAcessInterface
 	@Override
 	public DTO<Client> read(int ID) 
 	{
-		return null;
+		Connection conn = null;
+		DTO<Client> dto = null;
+		Client client = null;
+		
+		try 
+		{	
+			conn = db.open();
+			
+			String sql = "SELECT * FROM nmtsaDB.CLIENT WHERE id = " + ID;
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			client = new Client(rs.getString(""), rs.getString(""), rs.getString(""), rs.getString(""), 
+					rs.getString(""), rs.getInt(""), rs.getString(""), rs.getString(""), rs.getString(""), 
+					rs.getString(""), rs.getString(""), rs.getString(""), rs.getString(""), rs.getString(""), 
+					rs.getString(""));
+			
+			dto = new DTO<Client>(rs.getFetchSize(), "", client);
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (conn != null)
+			{
+				db.close(conn);
+			}
+		}
+		return dto;
 	}
 
 	@Override
