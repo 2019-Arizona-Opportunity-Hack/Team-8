8c9eccdd996bdac4f53d2d3d98617d4f186eba03
Ruben Cerrato <37849206+RubenC-18@users.noreply.github.com>
2019-10-19 17:09:50 -0700

    Implemented create()

--- src/com/data/ClientDAO.java
+++ src/com/data/ClientDAO.java
@@ -2,24 +2,34 @@ package com.data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.model.Client;
 import com.model.DTO;
+import com.model.Database;
 
 public class ClientDAO implements ClientDataAccessInterface 
 {	
+	Database db = null;
+	
+	public ClientDAO()
+	{
+		db = new Database();
+	}
+	
 	@Override
 	public DTO<Client> read(int ID) 
 	{
+		Connection conn = null;
 		DTO<Client> dto = null;
 		Client client = null;
 		
 		try 
 		{	
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/nmtsadb", "root", "root");
+			conn = db.open();
 			
 			String sql = "SELECT * FROM client WHERE id = " + ID;
 			Statement stmt = conn.createStatement();
@@ -34,12 +44,14 @@ public class ClientDAO implements ClientDataAccessInterface
 			}
 			
 			dto = new DTO<Client>(rs.getRow(), ((client == null) ? "FAILURE" : "OK"), client);
-			
-			conn = null;
 		} 
 		catch (Exception e) 
 		{
 			e.printStackTrace();
+		}
+		finally
+		{
+			db.close(conn);
 		}
 		return dto;
 	}
@@ -47,7 +59,52 @@ public class ClientDAO implements ClientDataAccessInterface
 	@Override
 	public DTO<Client> create(Client client) 
 	{
-		return null;
+		Connection conn = null;
+		DTO<Client> dto = null;
+		
+		try
+		{
+			conn = db.open();
+			
+			PreparedStatement sql = conn.prepareStatement(
+					"INSERT INTO `client` (`id`, `time`, `first`, `last`, `birthday`, "
+					+ "`phoneNumber`, `diagnosis`, `parentA`, `parentB`, `email`, "
+					+ "`address`, `reason`, `funding`, `availableDay`, `availableTime`, `notes`) VALUES "
+					+ "(NULL, '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?', '?')"
+					);
+			
+			sql.setString(1, client.getTimeStamp());
+			sql.setString(2, client.getFirstName());
+			sql.setString(3, client.getLastName());
+			sql.setString(4, client.getBirthDate());
+			sql.setString(5, client.getPhoneNumber());
+			sql.setString(6, client.getDiagnosis());
+			sql.setString(7, client.getParent1());
+			sql.setString(8, client.getParent2());
+			sql.setString(9, client.getEmail());
+			sql.setString(10, client.getAddress());
+			sql.setString(11, client.getReason());
+			sql.setString(12, client.getFunding());
+			sql.setString(13, client.getAvailableDay());
+			sql.setString(14, client.getAvailableTime());
+			sql.setString(15, client.getNotes());
+			
+			ResultSet rs = sql.executeQuery();
+			
+			int rowCount = 0;
+			while(rs.next()) { rowCount++; }
+			
+			dto = new DTO<Client>(rowCount, ((rowCount != 0) ? "OK" : "FAILURE"), client);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			db.close(conn);
+		}
+		return dto;
 	}
 
 	@Override
